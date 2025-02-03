b864ab1e275305ce497efac17e9e8b2bb43b241a
a <@a>
2025-02-03 11:04:13 -0500

    updated code

---
+++ src/Card.java
@@ -0,0 +1,159 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Card implements Comparable<Card>{
+    String line;
+    boolean jack;
+    public Card(String line, boolean jack){
+        this.line = line;
+        this.jack = jack;
+    }
+
+    public void displayDeck(){
+        System.out.println(line);
+    }
+
+    public String determineLine(){
+        HashMap<String, Integer> a = new HashMap<>();
+        for (String i: line.substring(0, line.indexOf("|")).split(",")){
+            if (a.containsKey(i)){
+                a.put(i, (a.get(i) + 1));
+            }
+            else {
+                a.put(i, 1);
+            }
+        }
+        if (a.size() == 1){
+            return "fiveOfAKind";
+        }
+        else if (a.size() == 2){
+
+            if (a.containsValue(4)){
+                return "fourOfAKind";
+            }
+
+            else {
+                return "fullHouse";
+            }
+        }
+        else if (a.size() == 3){
+
+            if (a.containsValue(3)){
+                return "threeOfAKind";
+            }
+            else {
+                return "twoPair";
+            }
+        }
+
+        else if (a.size() == 4){
+            return "onePair";
+        }
+
+        else {
+            return "highCard";
+        }
+    }
+    public String determineLineJack(){
+        HashMap<String, Integer> a = new HashMap<>();
+
+        for (String i: line.substring(0, line.indexOf("|")).split(",")){
+            if (a.containsKey(i)){
+                a.put(i, (a.get(i) + 1));
+            }
+            else {
+                a.put(i, 1);
+            }
+        }
+
+        if (a.containsKey("Jack")){
+            if (Objects.equals(mostCommonValue(a), "Jack")){
+                a.put(mostCommonValue(a), a.get(mostCommonValue(a)) + 1);
+            }
+            else {
+                int val = a.get("Jack");
+                a.remove("Jack");
+                a.put(mostCommonValue(a), a.get(mostCommonValue(a)) + val);
+            }
+        }
+
+
+        if (a.size() == 1){
+            return "fiveOfAKind";
+        }
+        else if (a.size() == 2){
+
+            if (a.containsValue(4)){
+                return "fourOfAKind";
+            }
+
+            else {
+                return "fullHouse";
+            }
+        }
+        else if (a.size() == 3){
+
+            if (a.containsValue(3)){
+                return "threeOfAKind";
+            }
+            else {
+                return "twoPair";
+            }
+        }
+
+        else if (a.size() == 4){
+            return "onePair";
+        }
+
+        else {
+            return "highCard";
+        }
+    }
+    public String mostCommonValue(HashMap<String, Integer> b){
+
+        int mostFrequent = -1;
+        String mostFrequentValue = "";
+        for (Map.Entry<String, Integer> e: b.entrySet()){
+            int count = e.getValue();
+            String value = e.getKey();
+            if (!Objects.equals(value, "Jack")){
+                if (count > mostFrequent){
+                    mostFrequent = count;
+                    mostFrequentValue = value;
+                }
+            }
+
+        }
+
+        return mostFrequentValue;
+    }
+    @Override
+    public int compareTo (Card card){
+        String[] ar = this.line.substring(0, this.line.indexOf("|")).split(",");
+        String[] br = card.line.substring(0, card.line.indexOf("|")).split(",");
+        String[] ranking = {"fiveOfAKind", "fourOfAKind", "fullHouse", "threeOfAKind", "twoPair", "onePair", "highCard"};
+
+        String[] ranking2 = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+        if (jack){
+            ranking2 = new String[]{"Ace", "King", "Queen", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "Jack"};
+
+        }
+        if (Arrays.asList(ranking).indexOf(card.determineLine()) == Arrays.asList(ranking).indexOf(this.determineLine())){
+            for (int i = 0; i < ar.length; i++){
+                if (Arrays.asList(ranking2).indexOf(br[i]) != Arrays.asList(ranking2).indexOf(ar[i])) {
+                    return Integer.compare(Arrays.asList(ranking2).indexOf(br[i]), Arrays.asList(ranking2).indexOf(ar[i]));
+                }
+            }
+        }
+        else {
+            return Integer.compare(Arrays.asList(ranking).indexOf(card.determineLine()), Arrays.asList(ranking).indexOf(this.determineLine()));
+        }
+        return 0;
+    }
+    public int returnBid(){
+        return Integer.valueOf(this.line.substring(this.line.indexOf("|") + 1));
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -10,98 +10,85 @@ public class Main {
     static int onePair;
     static int highCard;
     static int sum;
-    public static String eval(String line){
-        HashMap<String, Integer> a = new HashMap<>();
-        for (String i: line.substring(0, line.indexOf("|")).split(",")){
-            if (a.containsKey(i)){
-                a.put(i, (a.get(i) + 1));
-            }
-            else {
-                a.put(i, 1);
-            }
-        }
-        if (a.size() == 1){
-            fiveOfAKind += 1;
-            return "fiveOfAKind";
-        }
-        else if (a.size() == 2){
-            if (a.containsKey("Jack")){
-                fourOfAKind += 1;
-                return "fourOfAKind";
-            }
-            if (a.containsValue(4)){
-                fourOfAKind += 1;
-                return "fourOfAKind";
-            }
-            else {
-                fullHouse += 1;
-                return "fullHouse";
-            }
-        }
-        else if (a.size() == 3){
-            if (a.containsKey("Jack")){
-                threeOfAKind += 1;
-                return "threeOfAKind";
-            }
-            if (a.containsValue(3)){
-                threeOfAKind += 1;
-                return "threeOfAKind";
-            }
-            else{
-                twoPair += 1;
-                return "twoPair";
-            }
-        }
-        else if (a.size() == 4){
-            onePair += 1;
-            return "onePair";
-        }
-        else {
-            highCard += 1;
-            return "highCard";
-        }
-    }
-    public static void eval2(){
-        String[] ranking = {"fiveOfAKind", "fourOfAKind", "fullHouse", "threeOfAKind", "twoPair", "onePair", "highCard"};
-        String[] ranking2 = {"Ace", "King", "Queen", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "Jack"};
 
-        String[] xr = new String[re.textToArray("src/re.txt").length];
-        for (int i = 0; i < re.textToArray("src/re.txt").length; i ++){
-            xr[i] = re.textToArray("src/re.txt")[i];
-        }
-        Arrays.sort(xr, (a, b) -> {
-            String[] ar = a.substring(0, a.indexOf("|")).split(",");
-            String[] br = b.substring(0, b.indexOf("|")).split(",");
-            if (eval(b).compareTo(eval(a)) == 0){
-                for (int i = 0; i < ar.length; i++){
-                    if (Arrays.asList(ranking2).indexOf(ar[i]) != Arrays.asList(ranking2).indexOf(br[i])){
-                        return Integer.compare(Arrays.asList(ranking2).indexOf(br[i]), Arrays.asList(ranking2).indexOf(ar[i]));
-                    }
-                }
-            }
-            else {
-                return Integer.compare(Arrays.asList(ranking).indexOf(eval(b)), Arrays.asList(ranking).indexOf(eval(a)));
-            }
-            return 0;
-        });
-        for (int i = 0; i < xr.length; i++){
-            sum += Integer.valueOf(xr[i].substring(xr[i].indexOf("|") + 1)) * (i + 1);
-        }
-    }
+//    public static void eval2(){
+//        String[] ranking = {"fiveOfAKind", "fourOfAKind", "fullHouse", "threeOfAKind", "twoPair", "onePair", "highCard"};
+//        String[] ranking2 = {"Ace", "King", "Queen", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "Jack"};
+//
+//        String[] xr = new String[re.textToArray("src/re.txt").length];
+//        for (int i = 0; i < re.textToArray("src/re.txt").length; i ++){
+//            xr[i] = re.textToArray("src/re.txt")[i];
+//        }
+//        Arrays.sort(xr, (a, b) -> {
+//            String[] ar = a.substring(0, a.indexOf("|")).split(",");
+//            String[] br = b.substring(0, b.indexOf("|")).split(",");
+//            if (eval(b).compareTo(eval(a)) == 0){
+//                for (int i = 0; i < ar.length; i++){
+//                    if (Arrays.asList(ranking2).indexOf(ar[i]) != Arrays.asList(ranking2).indexOf(br[i])){
+//                        return Integer.compare(Arrays.asList(ranking2).indexOf(br[i]), Arrays.asList(ranking2).indexOf(ar[i]));
+//                    }
+//                }
+//            }
+//            else {
+//                return Integer.compare(Arrays.asList(ranking).indexOf(eval(b)), Arrays.asList(ranking).indexOf(eval(a)));
+//            }
+//            return 0;
+//        });
+//        for (int i = 0; i < xr.length; i++){
+//            System.out.println(xr[i] + eval(xr[i]));
+//            sum += Integer.valueOf(xr[i].substring(xr[i].indexOf("|") + 1)) * (i + 1);
+//        }
+//    }
 
     public static void main(String[] args) {
-        for (String i: re.textToArray("src/re.txt")){
-            eval(i);
+        Card[] c = createDeck("src/re.txt");
+        for (Card i: c){
+            if (Objects.equals(i.determineLineJack(), "fiveOfAKind")){
+                fiveOfAKind += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "fourOfAKind")){
+                fourOfAKind += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "threeOfAKind")){
+                threeOfAKind += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "fullHouse")){
+                fullHouse += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "twoPair")){
+                twoPair += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "onePair")){
+                onePair += 1;
+            }
+            if (Objects.equals(i.determineLineJack(), "highCard")){
+                highCard += 1;
+            }
+            System.out.println(i.line + i.determineLineJack());
         }
-        System.out.printf("Number of five of a kind hands:" +  fiveOfAKind);
-        System.out.printf("\nNumber of full house hands: " + fullHouse);
-        System.out.printf("\nNumber of four of a kind hands: " +  fourOfAKind);
-        System.out.printf("\nNumber of three of a kind hands: " + threeOfAKind);
-        System.out.printf("\nNumber of two pair hands: " + twoPair);
-        System.out.printf("\nNumber of one pair hands: " +  onePair);
-        System.out.printf("\nNumber of high card hands: " +  highCard + "\n");
-        eval2();
-        System.out.printf("Total Bid Value: %d", sum);
+        for (int i = 0; i < c.length; i++){
+            sum += (i + 1) * c[i].returnBid();
+        }
 
+        System.out.printf("Number of five of a kind hands: %d\n", fiveOfAKind);
+        System.out.printf("Number of full house hands: %d\n", fullHouse);
+        System.out.printf("Number of four of a kind hands: %d\n", fourOfAKind);
+        System.out.printf("Number of three of a kind hands: %d\n", threeOfAKind);
+        System.out.printf("Number of two pair hands: %d\n", twoPair);
+        System.out.printf("Number of one pair hands %d\n", onePair);
+        System.out.printf("Number of high card hands %d\n", highCard);
+        System.out.printf("Total Bid Value: %d\n", sum);
+
+
+    }
+    public static Card[] createDeck(String fileLocation){
+        String[] arr = re.textToArray(fileLocation);
+        int length = arr.length;
+        Card[] c = new Card[length];
+        for (int i = 0; i < arr.length; i++){
+            c[i] = new Card(arr[i], true);
+        }
+        Arrays.sort(c);
+        return c;
     }
 }
